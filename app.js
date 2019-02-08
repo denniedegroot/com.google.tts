3d587d837d55710a2b2d2fc03239b80fcca90fa0
Dennie de Groot <mail@denniedegroot.nl>
2019-02-08 15:47:45 +0100

    app: find current ip of device

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ class App extends Homey.App {
     onInit() {
         console.log(`${Homey.manifest.id} running...`);
 
-        let foundServers = [];
+        let FoundDevices = [];
         let browser = mdns.createBrowser(mdns.tcp('googlecast'));
         let ttsAction = new Homey.FlowCardAction('tts');
 
-        foundServers['Broadcast'] = {name: 'Broadcast', description: 'Broadcast to all devices'};
+        FoundDevices['Broadcast'] = {name: 'Broadcast', description: 'Broadcast to all devices'};
 
         browser.on('ready', function onReady() {
             browser.discover();
@@ -47,13 +47,13 @@ class App extends Homey.App {
                 }
 
                 if (data.type[0].name == 'googlecast')
-                    foundServers[data.fullname] = db;
+                    FoundDevices[data.fullname] = db;
             }
         });
 
         ttsAction.register().registerRunListener((args, state) => {
             return new Promise((resolve, reject) => {
-                let device = new googleTTS(args.device.host, args.language);
+                let device = new googleTTS(null, args.language);
 
                 device.setTtsTimeout(5000);
 
@@ -62,31 +62,40 @@ class App extends Homey.App {
 
                 if (args.device.name == 'Broadcast') {
                     device.getTtsUrl(args.text).then((url) => {
-                        for (let device_data in foundServers) {
-                            if (foundServers[device_data].name == 'Broadcast')
+                        for (let device_data in FoundDevices) {
+                            if (FoundDevices[device_data].name == 'Broadcast')
                                 continue;
 
-                            device.setIp(foundServers[device_data].host);
+                            device.setIp(FoundDevices[device_data].host);
                             device.audio(url, (result) => {
                                 resolve();
                             });
                         }
                     });
                 } else {
+                    device.setIp(this.findDeviceIP(FoundDevices, args.device));
                     device.tts(args.text, (result) => {
                         resolve();
                     });
                 }
             });
         }).getArgument('device').registerAutocompleteListener((query, args) => {
-            let foundDevices = []
+            let devices = []
 
-            for (let device_data in foundServers)
-                if (foundServers[device_data].name.toLowerCase().indexOf(query.toLowerCase()) > -1 || foundServers[device_data].description.toLowerCase().indexOf(query.toLowerCase()) > -1)
-                    foundDevices.push(foundServers[device_data]);
+            for (let device_data in FoundDevices)
+                if (FoundDevices[device_data].name.toLowerCase().indexOf(query.toLowerCase()) > -1 || FoundDevices[device_data].description.toLowerCase().indexOf(query.toLowerCase()) > -1)
+                    devices.push(FoundDevices[device_data]);
 
-            return Promise.resolve(foundDevices);
+            return Promise.resolve(devices);
         });
+    }
+
+    findDeviceIP(FoundDevices, device) {
+        for (let device_data in FoundDevices)
+            if (FoundDevices[device_data].name == device.name)
+                return FoundDevices[device_data].host;
+
+        return "0.0.0.0";
     }
 
 }
