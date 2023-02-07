42c9d6454f7a7200a990b819b65f1ce4d4437ac6
Martin Verbeek <dudz40@gmail.com>
2023-02-07 21:53:45 +0100

    3.0.0

--- app.js
+++ app.js
@@ -5,14 +5,6 @@ const googleTTS = require('google-home-audio-tts');
 
 class App extends Homey.App {
 
-    log() {
-        console.log.bind(this, '[log]').apply(this, arguments);
-    }
-
-    error() {
-        console.error.bind(this, '[error]').apply(this, arguments);
-    }
-
     onInit() {
         console.log(`${Homey.manifest.id} running...`);
 
